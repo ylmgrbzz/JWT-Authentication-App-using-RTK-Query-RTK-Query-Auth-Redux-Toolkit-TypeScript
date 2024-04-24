a32d2e060dd93a548e7717e5d37182fa9f8d8a8f
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-24 21:57:03 +0300

    Update index.css and App.test.tsx, remove unused imports

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,120 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { MDBInput } from "mdb-react-ui-kit";
+
+const initialState = {
+  email: "",
+  password: "",
+  firstName: "",
+  lastName: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [formValues, setFormValues] = useState(initialState);
+  const [showRegister, setShowRegister] = useState(false);
+  const { email, password, firstName, lastName, confirmPassword } = formValues;
+  const navigate = useNavigate();
+
+  return (
+    <section className="vh-100 gradient-custom">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-4 text-center">
+                <div className="mb-md-5 mt-md-4 pb-5">
+                  <h2 className="fw-bold mb-2 text-uppercase">
+                    {showRegister ? "Register" : "Login"}
+                  </h2>
+                  <p className="text-white-50 mb-5">
+                    {showRegister
+                      ? "Register to access your dashboard"
+                      : "Login to access your dashboard"}
+                  </p>
+
+                  <div className="form-outline form-white mb-4">
+                    <MDBInput
+                      label="Email"
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={(e) =>
+                        setFormValues({
+                          ...formValues,
+                          email: e.target.value,
+                        })
+                      }
+                      className="form-control form-control-lg"
+                    />
+                  </div>
+                  <div className="form-outline form-white mb-4">
+                    <MDBInput
+                      label="password"
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={(e) =>
+                        setFormValues({
+                          ...formValues,
+                          password: e.target.value,
+                        })
+                      }
+                      className="form-control form-control-lg"
+                    />
+                  </div>
+                  {!showRegister ? (
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      onClick={() => navigate("/dashboard")}
+                    >
+                      Login
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      onClick={() => navigate("/dashboard")}
+                    >
+                      Register
+                    </button>
+                  )}
+                </div>
+
+                <div>
+                  <p className="mb-0">
+                    {!showRegister ? (
+                      <>
+                        Not registered?{" "}
+                        <button
+                          className="btn btn-link"
+                          onClick={() => setShowRegister(true)}
+                        >
+                          Register
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        Already registered?{" "}
+                        <button
+                          className="btn btn-link"
+                          onClick={() => setShowRegister(false)}
+                        >
+                          Login
+                        </button>
+                      </>
+                    )}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Auth;

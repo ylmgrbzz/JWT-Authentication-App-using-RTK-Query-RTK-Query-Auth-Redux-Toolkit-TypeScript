4f1a4ff50d43dfe00691fb700002fe17c7dacc4e
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-24 23:06:41 +0300

    Update Dashboard component to include user name and logout functionality

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useAppSelector } from "../app/hooks";
+import { useNavigate } from "react-router-dom";
+import { selectAuth } from "../features/authSlice";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { name } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  return (
+    <section className="vh-100 gradient-custom">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div className="card" style={{ borderRadius: "15px" }}>
+              <div className="card-body p-5 text-center">
+                <h2 className="fw-bold">Welcome {name}</h2>
+                <button
+                  className="btn btn-primary btn-lg"
+                  onClick={() => navigate("/auth")}
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
